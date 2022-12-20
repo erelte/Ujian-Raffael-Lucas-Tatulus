6f677fcc00f8659d579d2eb62e905fbcc6ed1b30
erelte <raffaeltatulus@gmail.com>
2022-12-20 16:47:36 +0700

    Update soal nomor 3

--- Soal 3 - Aplikasi Info Bitcoin/bitcoin/src/App.js
+++ Soal 3 - Aplikasi Info Bitcoin/bitcoin/src/App.js
@@ -8,7 +8,10 @@ import {
   Link
 } from "react-router-dom";
 import axios from "axios";
+import { StyleSheet, Text, View ,TextInput, SafeAreaView  } from 'react-native';
+
 const infoBitcoin = "https://www.blockchain.com/ticker";
+const konvBitcoin = "https://www.blockchain.com/";
 
 export default function App() {
   return (
@@ -98,11 +101,66 @@ function Info() {
 }
 
 function RtB() {
-    return (
-          <h2>React Form Validation Demo</h2>
-    );
+  const [post, setPost] = React.useState(null);
+  const [usd, setUsd] = React.useState(null);
+
+  const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
+  function handleChange(event) {
+    var usd = event.target.value / 14000;
+    setUsd(usd);
+    axios.get(konvBitcoin + "tobtc?currency=USD&value=" + usd).then((response) => {
+      setPost(response.data);
+    });
+    console.log(usd);
+  }
+  return (
+    <SafeAreaView>
+      <h2>Konversi Rupiah ke Bitcoin</h2>
+      <h2>Kurs 1 USD = 14.000</h2>
+      <input  style={styles.input} name="firstName" onChange={handleChange} />
+      <h1>RP 14000 = BTC {post}</h1>
+    </SafeAreaView>
+  );
+
 }
 
 function BtR() {
-  return <h2>Konversi Bitcoin ke Rupiah</h2>;
+  const [post, setPost] = React.useState(null);
+  const [rp, setRp] = React.useState(null);
+
+  const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
+  React.useEffect(() => {
+    axios.get(infoBitcoin).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+  function handleChange(event) {
+    var btc = event.target.value;
+    var usd = post.USD.sell * btc;
+    setRp(usd * 14000);
+    console.log(usd);
+  }
+  return (
+    <SafeAreaView>
+      <h2>Konversi Bitcoin ke Rupiah</h2>
+      <h2>Kurs 1 USD = 14.000</h2>
+      <input  style={styles.input} name="firstName" onChange={handleChange} />
+      <h1>BTC 1 = RP {rp}</h1>
+    </SafeAreaView>
+  );
+
 }
