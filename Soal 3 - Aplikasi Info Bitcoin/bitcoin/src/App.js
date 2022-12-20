470bb837c149c75dc3157f54204584cad3143cba
erelte <raffaeltatulus@gmail.com>
2022-12-20 15:48:24 +0700

    Update soal 3

--- Soal 3 - Aplikasi Info Bitcoin/bitcoin/src/App.js
+++ Soal 3 - Aplikasi Info Bitcoin/bitcoin/src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import axios from "axios";
+const infoBitcoin = "https://www.blockchain.com/ticker";
 
 export default function App() {
   return (
@@ -13,13 +15,13 @@ export default function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/">Info</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/RtB">Konversi Rupiah ke Bitcoin</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/BtR">Bitcoin ke Rupiah</Link>
             </li>
           </ul>
         </nav>
@@ -27,14 +29,14 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
+          <Route path="/RtB">
             <About />
           </Route>
-          <Route path="/users">
+          <Route path="/BtR">
             <Users />
           </Route>
           <Route path="/">
-            <Home />
+            <Info />
           </Route>
         </Switch>
       </div>
@@ -42,14 +44,61 @@ export default function App() {
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
+function Info() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(infoBitcoin).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  console.log(post);
+  if (!post) return null;
+
+  return (
+    <div>
+      <h1>Harga Bitcoin Hari ini</h1>
+      <table style={{"borderWidth":"1px", 'borderColor':"#aaaaaa", 'borderStyle':'solid'}}>
+        <tr>
+          <th>Mata Uang</th>
+          <th>Harga Beli Bitcoin</th>
+          <th>Harga Jual Bitcoin</th>
+        </tr>
+        <tr>
+          <td>Dollar Australia</td>
+          <td>{post.AUD.buy}</td>
+          <td>{post.AUD.sell}</td>
+        </tr>
+        <tr>
+          <td>Euro Eropa</td>
+          <td>{post.EUR.buy}</td>
+          <td>{post.EUR.sell}</td>
+        </tr>
+        <tr>
+          <td>Poundsterling Inggris</td>
+          <td>{post.GBP.buy}</td>
+          <td>{post.GBP.sell}</td>
+        </tr>
+        <tr>
+          <td>Yen Jepang</td>
+          <td>{post.JPY.buy}</td>
+          <td>{post.JPY.sell}</td>
+        </tr>
+        <tr>
+          <td>Dollar Amerika</td>
+          <td>{post.USD.buy}</td>
+          <td>{post.USD.sell}</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 function About() {
-  return <h2>About</h2>;
+  return <h2>Konversi Rupiah ke Bitcoin</h2>;
 }
 
 function Users() {
-  return <h2>Users</h2>;
+  return <h2>Konversi Bitcoin ke Rupiah</h2>;
 }
